760ae4e74211ad53cef572dfc85748d52fdb9c9c
PedrosoCode <Gabrielpedrofc@gmail.com>
2023-10-26 13:36:36 -0300

    início do projeto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './bulma/css/bulma.min.css'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button class="button">Button</button>
         <a
           className="App-link"
           href="https://reactjs.org"
